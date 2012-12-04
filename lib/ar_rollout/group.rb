d9f7dd2031c1f5ee47ecbc04bffe45becaabcf62
Jonathan Clem <jonathan@jclem.net>
2012-12-04 10:44:44 -0800

    Group destroys rollouts after group destroy

--- lib/ar_rollout/group.rb
+++ lib/ar_rollout/group.rb
@@ -1,10 +1,16 @@
 class Group < ActiveRecord::Base
+  after_destroy :destroy_rollouts
+
   has_many :memberships, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validate :validate_not_defined
   attr_accessible :name
 
   private
+
+  def destroy_rollouts
+    Rollout.where(group: name).each(&:destroy)
+  end
 
   def validate_not_defined
     if ArRollout.defined_groups.include?(name.intern)
