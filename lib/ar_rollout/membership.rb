7a51e2f2c398582143b2bfe3c7953797398ff34d
Jonathan Clem <jonathan@jclem.net>
2012-12-03 17:28:26 -0800

    Membership validations, attr_accessibles

--- lib/ar_rollout/membership.rb
+++ lib/ar_rollout/membership.rb
@@ -1,6 +1,10 @@
 class Membership < ActiveRecord::Base
   self.table_name = :groups_users
 
+  attr_accessible :group_id, :user_id
+
   belongs_to :group
   belongs_to :user
+
+  validates :user_id, :group_id, presence: true
 end
