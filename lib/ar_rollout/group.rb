f9b630a3d1db13bb9e0b2bccfd7bd60f499a1256
Jonathan Clem <jonathan@jclem.net>
2012-12-03 18:12:23 -0800

    Group name can't be a defined group name

--- lib/ar_rollout/group.rb
+++ lib/ar_rollout/group.rb
@@ -1,5 +1,14 @@
 class Group < ActiveRecord::Base
   has_many :memberships, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+  validate :validate_not_defined
   attr_accessible :name
+
+  private
+
+  def validate_not_defined
+    if ArRollout.defined_groups.include?(name.intern)
+      errors.add(:name, "can't be a defined group name")
+    end
+  end
 end
